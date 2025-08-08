faf6c531be03f550b2da2d67721cfaea668a82de
josemauro <jose.lopes@dasa.com.br>
2025-08-07 21:59:53 -0300

    feat: change ui components

--- src/components/ui/Table.tsx
+++ src/components/ui/Table.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import clsx from "clsx";
 
 export function Table({ children }: { children: ReactNode }) {
   return (
@@ -9,10 +10,28 @@ export function Table({ children }: { children: ReactNode }) {
     </div>
   );
 }
-export const THead = ({ children }: { children: ReactNode }) => (
-  <thead className="bg-gray-100 text-left">{children}</thead>
+
+export const THead = ({ children, className, ...props }:
+  React.HTMLAttributes<HTMLTableSectionElement> & { children: ReactNode }) => (
+  <thead {...props} className={clsx("bg-gray-100 text-left", className)}>{children}</thead>
 );
-export const TBody = ({ children }: { children: ReactNode }) => <tbody>{children}</tbody>;
-export const TR = ({ children }: { children: ReactNode }) => <tr className="border-b">{children}</tr>;
-export const TH = ({ children }: { children: ReactNode }) => <th className="px-3 py-2 font-medium">{children}</th>;
-export const TD = ({ children }: { children: ReactNode }) => <td className="px-3 py-2">{children}</td>;
+
+export const TBody = ({ children, className, ...props }:
+  React.HTMLAttributes<HTMLTableSectionElement> & { children: ReactNode }) => (
+  <tbody {...props} className={clsx(className)}>{children}</tbody>
+);
+
+export const TR = ({ children, className, ...props }:
+  React.HTMLAttributes<HTMLTableRowElement> & { children: ReactNode }) => (
+  <tr {...props} className={clsx("border-b", className)}>{children}</tr>
+);
+
+export const TH = ({ children, className, ...props }:
+  React.ThHTMLAttributes<HTMLTableCellElement> & { children: ReactNode }) => (
+  <th {...props} className={clsx("px-3 py-2 font-medium", className)}>{children}</th>
+);
+
+export const TD = ({ children, className, title, ...props }:
+  React.TdHTMLAttributes<HTMLTableCellElement> & { children: ReactNode }) => (
+  <td {...props} title={title} className={clsx("px-3 py-2", className)}>{children}</td>
+);
